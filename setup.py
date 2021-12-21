742510effd841c94d2130480f2c74d3b32dc2eb0
Alex Nichol <unixpickle@gmail.com>
2021-12-21 11:33:13 -0800

    list sub-packages and extra files in setup.py
    This allows non-editable installs to work.

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from setuptools import setup
 
 setup(
     name="glide-text2im",
-    packages=["glide_text2im"],
+    packages=[
+        "glide_text2im",
+        "glide_text2im.clip",
+        "glide_text2im.tokenizer",
+    ],
+    package_data={
+        "glide_text2im.tokenizer": [
+            "bpe_simple_vocab_16e6.txt.gz",
+            "encoder.json.gz",
+            "vocab.bpe.gz",
+        ],
+        "glide_text2im.clip": ["config.yaml"],
+    },
     install_requires=[
         "Pillow",
         "attrs",
